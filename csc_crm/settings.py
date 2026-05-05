87b55625a64c3f0971d87de8a4bc0d899f4ce23e
gopikas0408 <gopikas04082005@gmail.com>
2026-05-05 11:14:48 +0530

    email set

--- csc_crm/settings.py
+++ csc_crm/settings.py
@@ -142,10 +142,10 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('gopikas04082005@gmail.com') 
+EMAIL_HOST_PASSWORD = os.environ.get('xqmfouunkhegpbq') 
 
-ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
+ADMIN_EMAIL = os.environ.get('gopikas04082005@gmail.com')
 
 
 # =========================================================
